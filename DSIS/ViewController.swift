c1db4f055b46f0c11c1d5c9bb146b80d2efdccea
seong8681 <hazzyseong@gmail.com>
2019-09-04 21:34:43 +0900

    Initial Commit

---
+++ DSIS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DSIS
+//
+//  Created by 주성호 on 04/09/2019.
+//  Copyright © 2019 주성호. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
